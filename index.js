2d1bd26035924254bdc03a495ca676711bdad361
Jiri Peterek <peterek@sourcelab.cz>
2020-03-18 10:55:31 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const options = {
+    SwitchCase: 1,
+    VariableDeclarator: { var: 1, let: 1, const: 'first' },
+    outerIIFEBody: 1,
+    MemberExpression: 1,
+    FunctionDeclaration: {
+        parameters: 2,
+        body: 1
+    },
+    FunctionExpression: {
+        parameters: 2,
+        body: 1
+    },
+    CallExpression: {
+        arguments: 1
+    },
+    ArrayExpression: 1,
+    ObjectExpression: 1,
+    ImportDeclaration: 1,
+    flatTernaryExpressions: false,
+    ignoreComments: false,
+    ignoredNodes: ['TemplateLiteral *']
+}
+
+module.exports = {
+    rules: {
+        'indent': [2, 4, options],
+        '@typescript-eslint/indent': [2, 4, options],
+    }
+}
